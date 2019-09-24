7e796b8499b86f83e0a46a3394d87c7da46d76ec
Daniel Passos <daniel@passos.me>
2019-09-24 16:00:51 -0300

    Improve register method

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import { Component } from 'react';
 import { Button } from 'reactstrap';
-import { PushRegistration } from '@aerogear/push'
+import { PushRegistration } from '@aerogear/push';
 import { ConfigurationService } from '@aerogear/core';
 import './App.css';
 
 import config from './mobile-service';
 
-class TestComponent extends Component {
-  register() {
-    console.log('Registering...12');
-    const reg = new PushRegistration(new ConfigurationService(config));
-    reg.register()
-      .then(() => console.log('registered'))
-      .catch(error =>
-        console.log('Failed: ', error.message, JSON.stringify(error)));
-  }
+class RegisterComponent extends Component {
+
+  state = {
+    registered: false
+  };
+
+  register = () => {
+    console.log('Registering...');
+    new PushRegistration(new ConfigurationService(config))
+      .register()
+      .then(() => {
+        console.log('Registered!');
+        this.setState({ registered: true });
+      })
+      .catch(error => console.log('Failed: ', error.message, JSON.stringify(error)));
+  };
 
   render() {
     return (
-    <Button onClick={this.register}>Click me please</Button>
+      <Button onClick={this.register} disabled={this.state.registered}>Register</Button>
     );
   }
 }
 
-
 function App() {
   return (
     <div className="App">
-      <TestComponent />
+      <RegisterComponent />
     </div>
   );
 }
