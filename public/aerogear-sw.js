e482545d66a5415bd285ea83bc87e53a5da946cd
Massimiliano Ziccardi <mziccard@redhat.com>
2019-09-27 15:05:35 +0200

    Fixing firefox serviceworker

--- public/aerogear-sw.js
+++ public/aerogear-sw.js
@@ -1,18 +1,16 @@
 self.addEventListener('push', event => {
   const data = event.data.text();
   const msg_chan = new MessageChannel();
-  clients.matchAll().then(clients => {
+  event.waitUntil(clients.matchAll({includeUncontrolled: true, type: 'window'}).then(clients => {
     console.log('clients', clients);
     clients.forEach(client => {
       console.log('posting');
       client.postMessage(data, [msg_chan.port2]);
-    })
+    });
     if (clients.length === 0) {
-      event.waitUntil(
-        self.registration.showNotification(data)
-      );
+      self.registration.showNotification(data)
     }
-  });
+  }));
 });
 
 self.addEventListener('install', function (event) {
